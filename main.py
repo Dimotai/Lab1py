7cec2e0c43cc8b5cda40d107544d34e6155220ca
Dimotai <dimotaimusic@gmail.com>
2020-09-01 18:45:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+temp = input("Enter temperature: ")
+cOrF = input("Enter unit in F/f or C/c: ")
+if(cOrF.lower() == "c"):
+    print(f"{temp}° in Celsius is equivalent to {(float(temp)*9/5)+32}° Fahrenheit.")
+elif(cOrF.lower() == "f"):
+    print(f"{temp}° in Fahrenheit is equivalent to {(float(temp)-32)*5/9}° Celsius.")
+else:
+    print(f"Invalid unit({cOrF})")
